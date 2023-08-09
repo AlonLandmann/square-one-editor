329af5e6fc0088b6026d76aaddbec94cc0468723
Alon Landmann <rafael.landmann@gmail.com>
2023-08-09 15:00:46 +0800

    add menu skeleton

--- components/main/MainRoot.js
+++ components/main/MainRoot.js
@@ -12,19 +12,52 @@ import Example from '@/components/units/Example'
 import Exercise from '@/components/units/Exercise'
 import ModuleProvider from '@/lib/ModuleProvider'
 import StackProvider from '@/lib/StackProvider'
+import pinToStack from '@/lib/pinToStack'
 import css from '@/scss/main/MainRoot.module.scss'
 
 export default function MainRoot({ module }) {
   const [stack, setStack] = useState([])
   const [stackInView, setStackInView] = useState(false)
+  const [menuInView, setMenuInView] = useState(true)
 
   useEffect(() => {
     setStackInView(true)
   }, [stack])
 
+  function handlePin(unit) {
+    setStack(prevStack => pinToStack(unit, prevStack))
+  }
+
   return (
     <ModuleProvider value={module}>
       <StackProvider value={[stack, setStack]}>
+        {menuInView &&
+          <div className={css.menu}>
+            <div className={css.menuContent}>
+              <div className={css.moduleTitle}>
+                {module.displayName}
+              </div>
+              {module.script.map(unit => (
+                <div key={uuid()}>
+                  {unit.type === 'heading' &&
+                    <div className={css.menuHeading}>
+                      {unit.content}
+                    </div>
+                  }
+                  {unit.number &&
+                    <div className={css.menuUnitItem} onClick={() => { handlePin(unit) }}>
+                      <div className={css.unitNumber}>{unit.number}</div>
+                      <div>{unit.type}</div>
+                    </div>
+                  }
+                </div>
+              ))}
+            </div>
+            <div className={css.hideMenu} onClick={() => { setMenuInView(false) }}>
+              <i className='bi bi-x-lg'></i>
+            </div>
+          </div>
+        }
         <div className={css.script}>
           {module.script.map(unit => (
             <div key={uuid()}>
